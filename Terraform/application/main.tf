e33b92878a51209961656a3b3b12bca9a4492673
Landon Babay <landon.babay@outlook.com>
2022-11-03 20:50:41 -0400

    removing old azure container registry password

--- Terraform/application/main.tf
+++ Terraform/application/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_web_app" "webapp" {
   virtual_network_subnet_id = data.azurerm_subnet.app_subnet.id
 
   app_settings = {  
-    "DOCKER_REGISTRY_SERVER_PASSWORD" = "TTDacQlqSIYO0c3r3WXa34h1dIyD/WaI"
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = "" #Insert the azure container registry password here
     "DOCKER_REGISTRY_SERVER_URL" = "demorailsazure.azurecr.io"
     "DOCKER_REGISTRY_SERVER_USERNAME" = "demorailsazure"
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
